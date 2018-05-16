8c6abbaa374e530ac0899f52e1185609ada76308
gabai23 <onuma_gabai@yahoo.co.jp>
2018-05-16 09:38:18 +0900

    github

---
+++ eclipse/practice/src/practice/Re.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Re {
+
+	/*ここはaddメソッドの定義*/
+	public static int add(int x,int y){
+		int ans = x+ y;
+		return ans;
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int ans= add(100,10);
+		System.out.println("100 + 10 = " + ans);
+	}
+
+}
---
+++ eclipse/practice/src/practice/Rensyuu.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Rensyuu {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int[] score= new int[5];
+		score[1] = 30;
+		System.out.println(score[1]);
+
+		
+
+}
+}
